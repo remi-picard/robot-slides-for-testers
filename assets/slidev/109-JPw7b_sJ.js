9d3510d700a5362f917f3d817691ec109c534c69
remi-picard <remi-picard@users.noreply.github.com>
2025-10-01 06:14:06 +0000

    deploy: ea401eae81cb263e5957f302ffd79d1a76da68b8

---
+++ assets/slidev/109-JPw7b_sJ.js
@@ -0,0 +1,13 @@
+import{_ as b}from"./CodeBlockWrapper-DyVZ4_OR.js";import{I as g}from"../default-BZFGXOqd.js";import{b as $,bz as p}from"../index-BPoptjxD.js";import{p as c,u,f as k}from"./context-BRLgj0Z2.js";import{b as h,o as x,w as m,g as e,e as v,m as C,a0 as s,v as P,x as y}from"../modules/vue-4mDv654O.js";import"../modules/unplugin-icons-IIIzr7mw.js";import"./index-DXBYSuAd.js";import"../modules/shiki-Cs_zAk09.js";const B={__name:"109",setup(d,{expose:n}){n(),c(p);const{$slidev:r,$nav:t,$clicksContext:o,$clicks:l,$page:a,$renderContext:f,$frontmatter:_}=u(),i={$slidev:r,$nav:t,$clicksContext:o,$clicks:l,$page:a,$renderContext:f,$frontmatter:_,InjectedLayout:g,get frontmatter(){return p},get useSlideContext(){return u},get _provideFrontmatter(){return c},get _frontmatterToProps(){return k}};return Object.defineProperty(i,"__isScriptSetup",{enumerable:!1,value:!0}),i}};function L(d,n,r,t,o,l){const a=b;return x(),h(t.InjectedLayout,P(y(t._frontmatterToProps(t.frontmatter,108))),{default:m(()=>[n[1]||(n[1]=e("h2",null,"List Comprehension",-1)),v(a,C({},{ranges:[]}),{default:m(()=>[...n[0]||(n[0]=[e("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[e("code",{class:"language-text"},[e("span",{class:"line"},[e("span",null,"*** Settings ***")]),s(`
+`),e("span",{class:"line"},[e("span",null,"Library     resources/robot_helper.py")]),s(`
+`),e("span",{class:"line"},[e("span")]),s(`
+`),e("span",{class:"line"},[e("span")]),s(`
+`),e("span",{class:"line"},[e("span",null,"*** Test Cases ***")]),s(`
+`),e("span",{class:"line"},[e("span",null,"Teste List Comprehension")]),s(`
+`),e("span",{class:"line"},[e("span",null,"    ${c3po}=    Build Immutable Robot    C3PO    Jaune")]),s(`
+`),e("span",{class:"line"},[e("span",null,"    ${r2d2}=    Build Immutable Robot    R2D2    Bleue")]),s(`
+`),e("span",{class:"line"},[e("span")]),s(`
+`),e("span",{class:"line"},[e("span",null,"    ${robots}=    Create List    ${c3po}    ${r2d2}")]),s(`
+`),e("span",{class:"line"},[e("span")]),s(`
+`),e("span",{class:"line"},[e("span",null,"    # Utilisez $robots (pas ${robots}) dans les expressions Python")]),s(`
+`),e("span",{class:"line"},[e("span",null,'    ${robots_jaunes}=    Evaluate    [r for r in $robots if r.color == "Jaune"]')])])],-1)])]),_:1},16)]),_:1},16)}const w=$(B,[["render",L],["__file","/@slidev/slides/109.md"]]);export{w as default};
